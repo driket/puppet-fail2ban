e7b9d7c775d4c1447f46425e898d8cd9132c981d
Cedric Villa <cedric.villa@gmail.com>
2014-05-03 19:00:23 +0200

    lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,28 +1,28 @@
 # configuer le produit ici
 class mv_fail2ban::params {
 
-	$packages = "fail2ban"
-	$service_name = "fail2ban"
-	$config_path = "/etc/fail2ban"
-	
-	# filters for services to monitor
-	$all_filters = [ 
-		{"name" => "ssh", 						"enabled" => "true", 	"config" => "" },
-		{"name" => "pam-generic", 		"enabled" => "true", 	"config" => "" },
-		{"name" => "xinetd-fail", 		"enabled" => "true",	"config" => "" },
-		{"name" => "ssh-ddos", 				"enabled" => "true", 	"config" => "" },
-		# Apache
-		{"name" => "apache", 					"enabled" => "false", "config" => "" },
-		{"name" => "apache-multiport","enabled" => "false",	"config" => "" },
-		{"name" => "apache-noscript",	"enabled" => "false", "config" => "" },
-		{"name" => "apache-overflows","enabled" => "false",	"config" => "" },
-		# FTP
-		{"name" => "vsftpd", 					"enabled" => "false", "config" => "" },
-		{"name" => "wuftpd", 					"enabled" => "false", "config" => "" },
-		# Mail 
-		{"name" => "postfix", 				"enabled" => "false", "config" => "" },
-		{"name" => "couriersmtp", 		"enabled" => "false", "config" => "" },
-		{"name" => "courierauth", 		"enabled" => "false", "config" => "" },
-		{"name" => "sasl", 						"enabled" => "false", "config" => "" },		
-	]
+  $packages = "fail2ban"
+  $service_name = "fail2ban"
+  $config_path = "/etc/fail2ban"
+
+  # filters for services to monitor
+  $all_filters = [
+    {"name" => "ssh",             "enabled" => "true",  "config" => "" },
+    {"name" => "pam-generic",     "enabled" => "true",  "config" => "" },
+    {"name" => "xinetd-fail",     "enabled" => "true",  "config" => "" },
+    {"name" => "ssh-ddos",        "enabled" => "true",  "config" => "" },
+    # Apache
+    {"name" => "apache",          "enabled" => "false", "config" => "" },
+    {"name" => "apache-multiport","enabled" => "false", "config" => "" },
+    {"name" => "apache-noscript", "enabled" => "false", "config" => "" },
+    {"name" => "apache-overflows","enabled" => "false", "config" => "" },
+    # FTP
+    {"name" => "vsftpd",          "enabled" => "false", "config" => "" },
+    {"name" => "wuftpd",          "enabled" => "false", "config" => "" },
+    # Mail
+    {"name" => "postfix",         "enabled" => "false", "config" => "" },
+    {"name" => "couriersmtp",     "enabled" => "false", "config" => "" },
+    {"name" => "courierauth",     "enabled" => "false", "config" => "" },
+    {"name" => "sasl",            "enabled" => "false", "config" => "" },
+  ]
 }
